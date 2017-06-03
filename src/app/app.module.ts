6f80229e594d8eb73151e0183f7dfe956a85b549
jryantap <jhericot@outlook.com>
2017-06-02 22:25:01 -0400

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { QuotesPage} from '../pages/quotes/quotes';
+import { CameraPage } from "../pages/camera/camera";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    QuotesPage,
+    CameraPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +22,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    QuotesPage,
+    CameraPage
   ],
   providers: [
     StatusBar,
