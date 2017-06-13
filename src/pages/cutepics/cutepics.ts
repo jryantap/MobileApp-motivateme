bd263f9784ca839f283c1974ce1c3e89b424104f
jryantap <jhericot@outlook.com>
2017-06-12 20:24:54 -0400

    commit

--- src/pages/cutepics/cutepics.ts
+++ src/pages/cutepics/cutepics.ts
@@ -9,12 +9,20 @@ import { Component } from '@angular/core';
 export class CutePicsPage {
   //set different local pics to array
   private pics: Array<any> = [
-    'assets/botw-1.png',
-    'assets/botw-2.png',
-    'assets/botw-3.png'
+    'assets/animpics/anim-1.jpg',
+    'assets/animpics/anim-2.jpg',
+    'assets/animpics/anim-3.jpg',
+    'assets/animpics/anim-4.jpg',
+    'assets/animpics/anim-5.jpeg',
+    'assets/animpics/anim-6.jpeg',
+    'assets/animpics/anim-7.jpg',
+    'assets/animpics/anim-8.jpg',
+    'assets/animpics/anim-9.jpeg',
   ];
   private currentPicture: string;
   private showAww: boolean = false;
+  private counter: number = 0;
+
 
   //this will randomize the array numbers
   randomize(){
@@ -24,8 +32,13 @@ export class CutePicsPage {
 
   //click to randomize pictures by calling randomize()
   onClickThis(){
-    this.showAww = true;
+    this.counter += 1;
+    if(this.counter == 3)
+    {
+      this.showAww = true;
+    }
     var newUrlIndex = this.randomize();
     this.currentPicture = this.pics[newUrlIndex];
+    //this.counter = 0;
   }
 }
