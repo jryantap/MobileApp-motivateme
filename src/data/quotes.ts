20b9ce2d1769fae13475ec68f31ec620c6f9bdf7
jryantap <jhericot@outlook.com>
2017-06-13 22:00:02 -0400

    commit

--- src/data/quotes.ts
+++ src/data/quotes.ts
@@ -17,6 +17,21 @@ export default [
         id: '3',
         person: 'Robert H. Schuller',
         text: 'What great thing would you attempt if you knew you could not fail?'
+      },
+      {
+        id: '4',
+        person: 'Jimmy Dean',
+        text:  'I can\'t change the direction of the wind, but I can adjust my sails to always reach my destination.'
+      },
+      {
+        id: '5',
+        person: 'Hellen Keller',
+        text: 'The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.'
+      },
+      {
+        id: '6',
+        person: 'Maya Angelou',
+        text: 'My mission in life is not merely to survive, but to thrive; and to do so with some passion, some compassion, some humor, and some style.'
       }
     ],
     icon: 'brush'
@@ -33,6 +48,26 @@ export default [
         id: '5',
         person: 'Robert Frost',
         text: 'Education is the ability to listen to almost anything without losing your temper.'
+      },
+      {
+        id: '6',
+        person: 'Eric Hoffer',
+        text: 'The hardest arithmetic to master is that which enables us to count our blessings.'
+      },
+      {
+        id: '7',
+        person: 'Anonymous',
+        text: 'Do not lower your goals to the height of your abilities. Instead, heighten your abilities to the level of your goals.'
+      },
+      {
+        id: '8',
+        person: 'Nelson Mandela',
+        text: 'It always seems impossible until it is done.'
+      },
+      {
+        id: '9',
+        person: 'Franz Kafka',
+        text: 'Anyone who keeps the ability to see beauty never grows old.'
       }
     ],
     icon: 'bicycle'
@@ -49,6 +84,26 @@ export default [
         id: '7',
         person: 'Norman Vincent Peale',
         text: 'Enthusiasm releases the drive to carry you over obstacles and adds significance to all you do.'
+      },
+      {
+        id: '8',
+        person: 'Charles Kingsley',
+        text: 'All we need to make us really happy is something to be enthusiastic about.'
+      },
+      {
+        id: '9',
+        person: 'Ralph Waldo Emerson',
+        text: 'Nothing great was ever achieved without enthusiasm.'
+      },
+      {
+        id: '10',
+        person: 'Mark Twain',
+        text: 'Let us endeavor so to live that when we come to die even the undertaker will be sorry.'
+      },
+      {
+        id: '11',
+        person: 'Dale Carnegie',
+        text: 'People rarely succeed unless they have fun in what they are doing.'
       }
     ],
     icon: 'battery-charging'
@@ -57,25 +112,30 @@ export default [
     category: 'motivational',
     quotes: [
       {
-        id: '8',
+        id: '10',
         person: 'Jim Rohn',
         text: 'Either you run the day or the day runs you.'
       },
       {
-        id: '9',
+        id: '11',
         person: 'Donna Brazile',
         text: 'I was motivated to be different in part because I was different.'
       },
       {
-        id: '10',
+        id: '12',
         person: 'Anonymous',
         text: 'The harder you work for something, the greater you will feel when you achieve it.'
 
       },
       {
-        id: '11',
+        id: '13',
         person: 'Stephen Curry',
         text: 'Success is not an accident.  Are the habits you have today on par with the dreams you have for tomorrow?'
+      },
+      {
+        id: '14',
+        person: 'Thomas Edison',
+        text: 'Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.'
       }
     ],
     icon: 'body'
