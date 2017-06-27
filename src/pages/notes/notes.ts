50071f3ffc8ab33c39345f151e52e2e23bf20bad
jryantap <jhericot@outlook.com>
2017-06-27 18:39:15 -0400

    commit

--- src/pages/notes/notes.ts
+++ src/pages/notes/notes.ts
@@ -4,7 +4,6 @@ import {ActionSheetController, AlertController, NavController, NavParams} from "
 import {SocialSharing} from "@ionic-native/social-sharing";
 import {ImagePicker} from "@ionic-native/image-picker";
 import {Storage} from '@ionic/storage';
-import {RedditPage} from "../reddit/reddit";
 import {EulaPage} from "../eula/eula";
 @Component({
   selector: 'page-notes',
